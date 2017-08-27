051be67d8b45e68e6a1a83ac1727b69c67888171
Code Raccoons <jacortez_94@hotmail.com>
2017-08-27 11:21:19 -0500

    lmao
    errror

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ var connector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(connector);
-https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/42bad151-2e3b-4441-9d4d-e53d096ce3e7?subscription-key=33c7ca06d81a4e50a32e11120bbda382&timezoneOffset=0&verbose=true&q=
-https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/42bad151-2e3b-4441-9d4d-e53d096ce3e7?subscription-key=33c7ca06d81a4e50a32e11120bbda382&timezoneOffset=0&verbose=true&q=
+
 // Setup LUIS
 const recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/42bad151-2e3b-4441-9d4d-e53d096ce3e7?subscription-key=33c7ca06d81a4e50a32e11120bbda382&timezoneOffset=0&verbose=false&q=');
 const intents = new builder.IntentDialog({ recognizers: [recognizer] });
