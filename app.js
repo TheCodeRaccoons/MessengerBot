9c8108bf03e33c0e32222a26a0a234b154c7a883
Code Raccoons <jacortez_94@hotmail.com>
2017-08-27 09:51:57 -0500

    update key
    just the key m8

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 
 // Setup LUIS
-const recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/42bad151-2e3b-4441-9d4d-e53d096ce3e7?subscription-key=33c7ca06d81a4e50a32e11120bbda382&verbose=true&timezoneOffset=0&q=');
+const recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/42bad151-2e3b-4441-9d4d-e53d096ce3e7?subscription-key=33c7ca06d81a4e50a32e11120bbda382&timezoneOffset=0&verbose=true&q=');
 const intents = new builder.IntentDialog({ recognizers: [recognizer] });
 
 
