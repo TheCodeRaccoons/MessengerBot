f9885520ae866453984b19137596fb6199e1a5c8
Code Raccoons <jacortez_94@hotmail.com>
2017-11-19 11:09:48 -0600

    asd
    asd

--- app.js
+++ app.js
@@ -1,85 +1,166 @@
 var restify = require('restify');
-var builder = require('botbuilder');
+var builder = require('botbuilder');  
+var request = require("request");
 
-// Setup Restify Server
 var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 4464, function() {
+    console.log('%s Listening to %s', server.name, server.url);
+});
 
 
-// Create chat bot
 var connector = new builder.ChatConnector({
-    
-    appId: '731141aa-6e6f-4e10-9881-052b64ff160d',
-
+    appId: '731141aa-6e6f-4e10-9881-052b64ff160d', 
     appPassword: 'NMgMW0BJcxpGoho6AURqgGV'
-
 });
-
+ 
 var bot = new builder.UniversalBot(connector);
+server.post('api/messages', connector.listen());
 
-// Setup LUIS
-const recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/42bad151-2e3b-4441-9d4d-e53d096ce3e7?subscription-key=33c7ca06d81a4e50a32e11120bbda382&timezoneOffset=0&verbose=false&q=');
-const intents = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', [
+    function(session, results, next) {
+        if (!session.userData.nombre) {
+            builder.Prompts.text(session, 'Hola, Cual es tu nombre?');
+        } else {
+            next();
+        }
+    },
+    function(session, results) {
+        if (results.response) {
+            let msj = results.response;
+            session.userData.nombre = msj;
+        }
+        session.send(`Hola ${session.userData.nombre} !`);
 
+        session.beginDialog('/preguntaDatos');
 
-/************************************
-** Intents and actions taken by the**
-** AI depending on the request made**
-*************************************/ 
-
-//SAP Workflow
-intents.matches('saludo', function (session, results) {
-    session.send('Hola, soy Sigmi. El robot ayudante de Sigma ¿En qué te puedo ayudar?'); 
-});
-
-
-//Change password
-intents.matches('chPw', function (session, results) {
-    session.send('¿Con la contrasena de que cuenta tienes problema?');
-    session.send('Sap, Active Directory, Service Desk, Sigma'); 
-});
-
-
-//SAP Workflow
-intents.matches('psSap', function (session, results) {
-    session.send('Deacuerdo, para poder reestablecer o cambiar tu contrasena de SAP es necesario entrar a esta liga');
-    session.send('https://www.youtube.com/watch?v=hQcLd-T30g4&t=1285s'); 
-});
+    }
+]);
 
 
-//AD Workflow
-intents.matches('psAD', function (session, results) {
-    session.send('Veamos, para reestablecer o cambiar tu contrasena de Active Directory es necesario entrar a esta liga');
-    session.send('https://es.surveymonkey.com/welcome/sem/'); 
-    session.send('Al llenar el formulario da click en Aceptar y Listo!');
-});
+bot.dialog('/preguntaDatos', [
+    function(session, results) { 
+        builder.Prompts.text(session, 'A donde quieres viajar?'); 
+    },
+    function(session, results) {
+        if (results.response) {
+            let msj = results.response;
+            session.userData.destino = msj;
+        }
+        builder.Prompts.text(session,` ${session.userData.destino} Excelente eleccion, cual seria la ciudad de origen?`);
+    },
+    function(session, results) {
+        if (results.response) {
+            let msj = results.response;
+            session.userData.origen = msj;
+        }
+        session.send(`un viaje a ${session.userData.destino} saliendo de ${session.userData.origen} muy bien`);
+        builder.Prompts.text(session,'en que fecha quisieras salir (Porfavor ingresar la fecha en el sig formato dd-mm-aaaa)');
+    },
+    function(session, results) {
+        if (results.response) {
+            let msj = results.response;
+            session.userData.fecha = msj;
+        }
+        session.send(`Perfecto!  ${session.userData.origen} - ${session.userData.destino} saliendo el dia  ${session.userData.fecha}`);
+ 
+        session.beginDialog('/preguntaSalida');
+    }
+]);
 
 
-//Unlock User
-intents.matches('asSap', function (session, results) {
-    session.send('podrias darnos tu correo electronico para enviar una liga de restablecimiento?'); 
-});
+bot.dialog('/preguntaSalida', [
+    function(session) {
+        builder.Prompts.text(session, 'En que te gustaria viajar? autobus o avion?');
+    },
+    function(session, results) {
+        if (results.response) {
+            if(results.response == "autobus"){
+                let msj = "bus";
+                session.userData.transporte = msj;
+        }
+            if(results.response == "avion"){
+                let msj = "plane";
+                session.userData.transporte = msj;
+            }
+            
+        }
+            
+            //Use the data inputed by the user to generate the post body
+            var options = { method: 'POST',
+                url: 'https://www.reservamos.mx/api/v2/search.json',
+                headers: 
+                { 'postman-token': '3a89394d-f0ab-6783-89ad-d3db73d644f7',
+                    'cache-control': 'no-cache',
+                    'authorization': 'Token token=f986373f694ffdf9cfbae45ba23f3d8c',
+                    'content-type': 'application/json' },
+                body: 
+                {   origin:`${session.userData.origen.toString()}`,
+                    destination: `${session.userData.destino.toString()}`,
+                    date: `${session.userData.fecha.toString() }`
+                },
+                json: true };
+
+            //Request the ID depending on the data the user has entered
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
+                if (!body.search) { 
+                    console.log(body)
+                    session.send(`No contamos con viajes de ${session.userData.origen} a ${session.userData.destino}`);
+                }else{
+                    session.userData.SearchId = body.search.id;
+                    console.log(body)
+                } 
+                console.log( session.userData.origen.toString()+ " " + session.userData.destino.toString() + " " + session.userData.fecha.toString() +" "+ session.userData.transporte.toString() + " " + session.userData.SearchId)
+            
+                session.send(`Saliendo de ${session.userData.origen} contamos con las siguientes opciones:`);
 
 
-//Unlock User
-intents.matches('ubUs', function (session, results) {
-    session.send('Lamentamos escuchar eso ¿En que cuenta te encuentras bloqueado?');
-    session.send('Sap, Active Directory');  
-});
+                //Generate the data gathering
+                var optionsGet = {
+                    method: 'GET',
+                    url: `https://www.reservamos.mx/api/v2/search/${session.userData.SearchId}?type=${session.userData.transporte}`,
+                    qs: { type: session.userData.transporte },
+                    headers: {'content-type': 'application/json'}, 
+                    json: true };
 
-
-//Set default response
-intents.onDefault(builder.DialogAction.send('Lo siento no entendi a que te refieres. buscas informacion sobre: problemas con la contrasena / problemas para reactivar tu cuenta / dar de alta a un usuario en sap?')); 
-
-
-//Every time a dialog is realized it will try to catch some of the intents
-bot.dialog('/', intents);
-
-
-//Tells the place where our posts are going to be handeled
-server.post('/api/messages', connector.listen());
-
-
-//Stablish the port and lovely print it on console (even if we aint gonna see it...)
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to http://localhost:', server.url); 
-});
+                //Send the Requestfor the travel
+                request(optionsGet, function (error, response, body) {
+                    if (error) throw new Error(error);
+                    console.log(body)
+                    if(session.userData.transporte == 'plane'){
+                        for(var line in body.flights){
+                            session.send(body.flights[line].id);
+                        }
+                    }
+                    if(session.userData.transporte == 'bus'){
+                        for(var line in body.lines){
+                            //session.send(body.lines[line].name);3
+                            var regiViaje ={
+                                name: `${body.lines[line].name}`,
+                                a_rating: `${body.lines[line].average_ratings}`,
+                                t_rating: `${body.lines[line].total_ratings}`,
+                                services:[`${body.lines[line].services[0]}`,`${body.lines[line].services[1]}`,`${body.lines[line].services[2]}`],
+                                logo:  `${body.lines[line].logo_url}`,
+                                url:`${body.lines[line].redirection_info.desktop.url}`
+                            
+                            }; 
+                                var msg = new builder.Message(session);
+                                msg.attachmentLayout(builder.AttachmentLayout.carousel)
+                                msg.attachments([
+                                    new builder.HeroCard(session)
+                                        .title(regiViaje.name)
+                                        .subtitle(`Rating: ${regiViaje.a_rating} Rating total: ${regiViaje.t_rating}`)
+                                        .text(`Servicios: ${regiViaje.services}`)
+                                        .images([builder.CardImage.create(session, regiViaje.logo)])
+                                        .buttons([
+                                            builder.CardAction.openUrl(session, regiViaje.url)
+                                        ])
+                                ]);
+                                // Show carousel
+                                session.send(msg).endDialog();   
+                        }
+                    }
+            });
+        });
+    }
+]);
